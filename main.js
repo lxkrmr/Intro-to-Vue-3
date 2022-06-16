7502237b6061b08af48d6ce495458eda8c8452b1
Alex <lxkrmr@icloud.com>
2022-06-16 11:10:33 +0200

    Added a description

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'This socks were worn by the fox.'
+        }
+    }
+})
