c2dfa25b5427584547d9e642ddfbe17674c16bef
Alex <lxkrmr@icloud.com>
2022-06-19 20:03:55 +0200

    Code Challegne: On Sale

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            onSale: true
         }
     },
     methods: {
@@ -19,6 +19,23 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        sale() {
+            return this.title  + ' is on sale'
         }
     }
 })
